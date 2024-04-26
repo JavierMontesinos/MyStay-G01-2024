903d10f15bdb55a8cf784bc753686dfb72a889ac
JavierMontesinos <j.montesinos@alumnos.upm.es>
2024-04-26 12:19:59 +0200

    repository

--- src/main/java/es/upm/dit/isst/mystayapi/repository/ClienteRepository.java
+++ src/main/java/es/upm/dit/isst/mystayapi/repository/ClienteRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import es.upm.dit.isst.mystayapi.model.Cliente;
 
+@Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
 	List<Cliente> findByPagado(boolean pagado);
 	Optional<Cliente> findByDNI(String dni);
