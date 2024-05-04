de574e3a185543b0047224f1ec8d6ee73979e34a
no0bie <abrilc255@gmail.com>
2024-05-04 16:34:09 +0200

    Migracion total

--- mystay-front/e2e/starter.test.js
+++ mystay-front/e2e/starter.test.js
@@ -14,10 +14,11 @@ describe('Login screen', () => {
   });
 
   
-  it('Login form should be visible', async () => {
-    await expect(element(by.id('dni-login'))).toBeVisible();
-    await expect(element(by.id('nhab-login'))).toBeVisible();
-    await expect(element(by.id('btn-login'))).toBeVisible();
+  it('Should login with provided credentials', async () => {
+    await element(by.id('dni-login')).typeText('12345');
+    await element(by.id('nhab-login')).typeText('101\n');
+    
+    await element(by.id('btn-login')).tap();
   });
 
 });
