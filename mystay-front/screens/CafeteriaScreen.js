22e6d41bb1bc01bf568c670dea8a2c1564a9cbad
no0bie <abrilc255@gmail.com>
2024-05-06 00:15:14 +0200

    Añadido servicio de cafeteria

--- mystay-front/screens/CafeteriaScreen.js
+++ mystay-front/screens/CafeteriaScreen.js
@@ -4,18 +4,49 @@ import { StyleSheet } from 'react-native';
 import CustomButton from '../components/CustomButton';
 import { TitleText } from '../components/CustomText';
 
+import { post, validJWT } from '../utils/Requests';
+import AuthContext from '../utils/AuthProvider';
+
 const CafeteriaScreen = () => {
+  const { signOut } = React.useContext(AuthContext);
+
+  const postService = async (nombre, label) => {
+    
+    const serviceData = {
+      nombre,
+      descripcion: "Comida a la habitación",
+      fecha: new Date().toISOString()
+    };
+
+    try {
+      await post("cliente/servicio", serviceData);
+      alert(`Se ha pedido correctamente ${label} a la habitación`)
+    } catch (error) {
+      if (validJWT(error.response?.data, signOut)) {
+        console.log(error)
+        if (error.response?.data){
+          alert(error.response?.data)
+        } else {
+          alert(error)
+        }
+      }
+
+      return false;
+    }
+
+  };
+
   return (
     <View style={styles.container}>
       <TitleText text={"SERVICIO DE CAFETERÍA"} />
       <View style={styles.buttonContainer}>
-        <CustomButton icon={""} text={"Desayuno"} style={styles.text} /> 
+        <CustomButton icon={""} text={"Desayuno"} style={styles.text} func={() => postService("Desayuno", 'el desayuno')} /> 
       </View>
       <View style={styles.buttonContainer}>
-        <CustomButton icon={""} text={"Comida"} />
+        <CustomButton icon={""} text={"Comida"} func={() => postService("Comida", 'la comida')} />
       </View>
       <View style={styles.buttonContainer}>
-        <CustomButton icon={""} text={"Cena"} />
+        <CustomButton icon={""} text={"Cena"} func={() => postService("Cena", 'la cena')}/>
       </View>
     </View>
   );
--- mystay-front/screens/ProfileScreen.js
+++ mystay-front/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import CustomButton from '../components/CustomButton';
 import { TitleText } from '../components/CustomText'
 
