a9405b631a66d850bf2696a36833f1daf2b3d41a
no0bie <abrilc255@gmail.com>
2024-05-06 02:31:54 +0200

    Arreglado los tests añadiendo sleeps

--- mystay-front/e2e/starter.test.js
+++ mystay-front/e2e/starter.test.js
@@ -14,7 +14,7 @@ const habitacion = {
 }
 
 const habitacion2 = {
-  "numero":  500,
+  "numero": 500,
   "tipo": "Double",
   "precio": 300.0,
   "id": 4000
@@ -46,38 +46,38 @@ const instance = axios.create({
 
 
 describe('Login screen', () => {
-    beforeAll(async () => {
+  beforeAll(async () => {
     try {
-      let response = await instance.post("/login", {dni: "admin", nhab: "admin"});
+      let response = await instance.post("/login", { dni: "admin", nhab: "admin" });
       admin = response.data.token;
 
       instance.defaults.headers.common['Authorization'] = `Bearer ${admin}`;
-      
-      response = await instance.post("/hoteles", hotel, {headers: {'Content-Type': 'application/json'}});
+
+      response = await instance.post("/hoteles", hotel, { headers: { 'Content-Type': 'application/json' } });
       let hotelTemp = response.data;
 
-      
-      response = await instance.post("/habitaciones", habitacion, {headers: {'Content-Type': 'application/json'}});
+
+      response = await instance.post("/habitaciones", habitacion, { headers: { 'Content-Type': 'application/json' } });
       let habitacionTemp = response.data;
 
       await instance.put(`/habitaciones/${habitacionTemp.id}/hotel/${hotelTemp.id}`);
-      
-      response = await instance.post("/clientes", cliente, {headers: {'Content-Type': 'application/json'}});
+
+      response = await instance.post("/clientes", cliente, { headers: { 'Content-Type': 'application/json' } });
       let clienteTemp = response.data;
-      
+
       await instance.put(`/clientes/${clienteTemp.id}/habitacion/${habitacionTemp.id}`);
-      
+
       await instance.put(`/clientes/${clienteTemp.id}/hotel/${hotelTemp.id}`);
-      
-      response = await instance.post("/habitaciones", habitacion2, {headers: {'Content-Type': 'application/json'}});
+
+      response = await instance.post("/habitaciones", habitacion2, { headers: { 'Content-Type': 'application/json' } });
       await instance.put(`/habitaciones/${response.data.id}/hotel/${hotelTemp.id}`);
-      
+
       clienteID = clienteTemp.id;
       habitacionID = habitacionTemp.id;
       habitacion2ID = response.data.id;
       hotelID = hotelTemp.id;
-      
-    } catch (error) { 
+
+    } catch (error) {
       console.error(error)
       throw error;
     }
@@ -91,6 +91,7 @@ describe('Login screen', () => {
 
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
   it('Should login with provided credentials', async () => {
@@ -100,7 +101,7 @@ describe('Login screen', () => {
 
     await element(by.id('dni-login')).typeText(cliente.dni);
     await element(by.id('nhab-login')).typeText(`${habitacion.numero}\n`);
-    
+
     await waitFor(element(by.id('btn-login'))).toBeVisible().withTimeout(2000);
 
     await element(by.id('btn-login')).tap();
@@ -119,13 +120,14 @@ describe('Profile screen', () => {
 
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
   it('Should be able to edit user info', async () => {
     await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('Edita tus datos personales')).tap();
-    
+
     await waitFor(element(by.text(cliente.nombre))).toBeVisible().withTimeout(2000);
     await element(by.text(cliente.nombre)).replaceText(nuevoNombre);
 
@@ -139,7 +141,7 @@ describe('Profile screen', () => {
 
   it('Should be able to persist user info changes', async () => {
     await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('Edita tus datos personales')).tap();
     await waitFor(element(by.text(nuevoNombre))).toBeVisible().withTimeout(2000);
   });
@@ -154,9 +156,10 @@ describe('Checkout screen', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
+
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
 
@@ -169,7 +172,7 @@ describe('Checkout screen', () => {
 
     await waitFor(element(by.text('Ver factura'))).toBeVisible().withTimeout(2000);
     await element(by.text('Ver factura')).tap();
-    
+
     await waitFor(element(by.text(`Pagado: ${cliente.gasto}`))).not.toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
   });
@@ -183,7 +186,7 @@ describe('Checkout screen', () => {
 
     await waitFor(element(by.text('Pagar'))).toBeVisible().withTimeout(2000);
     await element(by.text('Pagar')).tap();
-    
+
     await waitFor(element(by.id('bank'))).toBeVisible().withTimeout(2000);
     await element(by.id('bank')).typeText('1234567899876');
     await element(by.id('cvv')).typeText('012\n');
@@ -192,7 +195,7 @@ describe('Checkout screen', () => {
     await waitFor(element(by.text('OK'))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
 
-    await waitFor(element(by.text('Pagar'))).toBeVisible().withTimeout(2000);
+    await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
   });
 
   it('Should not be able to pay', async () => {
@@ -204,7 +207,7 @@ describe('Checkout screen', () => {
 
     await waitFor(element(by.text('Pagar'))).toBeVisible().withTimeout(2000);
     await element(by.text('Pagar')).tap();
-    
+
     await waitFor(element(by.id('bank'))).toBeVisible().withTimeout(2000);
     await element(by.id('bank')).typeText('1234567899876');
     await element(by.id('cvv')).typeText('012\n');
@@ -213,7 +216,7 @@ describe('Checkout screen', () => {
     await waitFor(element(by.text('OK'))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
 
-   await expect(element(by.text('PAGO DE LA CUENTA'))).toBeVisible(); 
+    await expect(element(by.text('PAGO DE LA CUENTA'))).toBeVisible();
   });
 
   it('Should be able to see factura', async () => {
@@ -225,7 +228,7 @@ describe('Checkout screen', () => {
 
     await waitFor(element(by.text('Ver factura'))).toBeVisible().withTimeout(2000);
     await element(by.text('Ver factura')).tap();
-    
+
     await waitFor(element(by.text(`Pagado: ${cliente.gasto}`))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
   });
@@ -239,9 +242,10 @@ describe('Reservas', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
+
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
 
@@ -253,15 +257,15 @@ describe('Reservas', () => {
     await element(by.text('Nueva reserva')).tap();
 
     await waitFor(element(by.text('Reserva'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('13')).tap();
     await element(by.text('19')).tap();
-    
+
     await element(by.text('19')).swipe('up', 'fast', 1);
-    
+
     await waitFor(element(by.id('android_picker'))).toBeVisible().withTimeout(2000);
     await element(by.id('android_picker')).tap();
-    
+
     await element(by.text(hotel.nombre)).tap();
     await element(by.id('btn-reserve')).tap();
 
@@ -273,7 +277,7 @@ describe('Reservas', () => {
 
   it('Should be able to persist the created reserve', async () => {
     await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('Consulta tus reservas')).tap();
 
     await waitFor(element(by.text(hotel.nombre))).toBeVisible().withTimeout(2000);
@@ -290,9 +294,10 @@ describe('Servicios', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
+
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
   it('Should be able to request services', async () => {
@@ -309,7 +314,7 @@ describe('Servicios', () => {
 
     // Almohadas
     await element(by.id('checkbox')).atIndex(2).tap();
-    
+
     await element(by.text('Submit')).tap();
 
     await waitFor(element(by.text('Servicios Seleccionados: Ropa de cama, Almohadas'))).toBeVisible().withTimeout(2000);
@@ -326,9 +331,10 @@ describe('Incidencias', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
+
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
   it('Should be able to create a incidence', async () => {
@@ -343,7 +349,7 @@ describe('Incidencias', () => {
     await waitFor(element(by.text('Enviar Incidencia'))).toBeVisible().withTimeout(2000);
     await element(by.text('Enviar Incidencia')).tap()
 
-    
+
     await waitFor(element(by.text('Incidencia enviada con éxito'))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
 
@@ -358,9 +364,10 @@ describe('Otros servicios', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
+
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
   it('Should be able to change room', async () => {
@@ -382,7 +389,7 @@ describe('Otros servicios', () => {
 
     await waitFor(element(by.text('Solicitar'))).toBeVisible().withTimeout(2000);
     await element(by.text('Solicitar')).tap();
-    
+
     await waitFor(element(by.text('OK'))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
 
@@ -411,9 +418,10 @@ describe('Transporte', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
+
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
   it('Should be able to request taxi', async () => {
@@ -423,7 +431,7 @@ describe('Transporte', () => {
 
     await waitFor(element(by.text('Taxi'))).toBeVisible().withTimeout(2000);
     await element(by.text('Taxi')).tap();
-    
+
     await waitFor(element(by.text('Se ha pedido correctamente el servicio de taxi'))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
   });
@@ -450,24 +458,25 @@ describe('Premium', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
+
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
   it('Should not be premium', async () => {
     await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('Edita tus datos personales')).tap();
-    
+
     await waitFor(element(by.text('No premium'))).toBeVisible().withTimeout(2000);
   });
 
   it('Should be able to become premium', async () => {
     await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('Hazte premium')).tap();
-    
+
     await waitFor(element(by.id('bank'))).toBeVisible().withTimeout(2000);
     await element(by.id('bank')).typeText('1234567899876');
     await element(by.id('cvv')).typeText('012\n');
@@ -481,17 +490,17 @@ describe('Premium', () => {
 
   it('Should be premium', async () => {
     await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('Edita tus datos personales')).tap();
-    
+
     await waitFor(element(by.text('Premium'))).toBeVisible().withTimeout(2000);
   });
 
   it('Should not be able to become premium', async () => {
     await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('Hazte premium')).tap();
-    
+
     await waitFor(element(by.id('bank'))).toBeVisible().withTimeout(2000);
     await element(by.id('bank')).typeText('1234567899876');
     await element(by.id('cvv')).typeText('012\n');
@@ -513,9 +522,10 @@ describe('Cafeteria', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
-  
+
   afterAll(async () => {
     await device.terminateApp();
+    await new Promise(resolve => setTimeout(resolve, 1000));
   })
 
   it('Should be able to request breakfast', async () => {
@@ -525,8 +535,8 @@ describe('Cafeteria', () => {
 
     await waitFor(element(by.text('Desayuno'))).toBeVisible().withTimeout(2000);
     await element(by.text('Desayuno')).tap();
-    
-    await waitFor(element(by.text('Se ha pedido correctamente el desayuno a la habitacion'))).toBeVisible().withTimeout(2000);
+
+    await waitFor(element(by.text(/^Se ha pedido correctamente.*$/))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
   });
 
@@ -537,8 +547,8 @@ describe('Cafeteria', () => {
 
     await waitFor(element(by.text('Comida'))).toBeVisible().withTimeout(2000);
     await element(by.text('Comida')).tap();
-    
-    await waitFor(element(by.text('Se ha pedido correctamente la comida a la habitacion'))).toBeVisible().withTimeout(2000);
+
+    await waitFor(element(by.text(/^Se ha pedido correctamente.*$/))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
   });
 
@@ -549,8 +559,8 @@ describe('Cafeteria', () => {
 
     await waitFor(element(by.text('Cena'))).toBeVisible().withTimeout(2000);
     await element(by.text('Cena')).tap();
-    
-    await waitFor(element(by.text('Se ha pedido correctamente la cena a la habitacion'))).toBeVisible().withTimeout(2000);
+
+    await waitFor(element(by.text(/^Se ha pedido correctamente.*$/))).toBeVisible().withTimeout(2000);
     await element(by.text('OK')).tap();
   });
 
@@ -575,7 +585,7 @@ describe('Logout', () => {
 
   it('Should be able to logout', async () => {
     await waitFor(element(by.text('MI PERFIL'))).toBeVisible().withTimeout(2000);
-    
+
     await element(by.text('Logout')).tap();
 
     await waitFor(element(by.id('btn-login'))).toBeVisible().withTimeout(2000);
